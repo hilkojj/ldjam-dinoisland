b817c37e22d636576bdc902cbaeae7d32192afef
hilkojj <hilkojj@outlook.com>
2023-10-01 00:40:30 +0200

    Respawn dodo when player is not nearby

--- assets/scripts/entities/level_room/DodoSpawner.lua
+++ assets/scripts/entities/level_room/DodoSpawner.lua
@@ -15,11 +15,30 @@ function create(spawner)
         },
         SphereColliderShape {
             radius = 0.5
-        },
+        }
     })
 
-    local dodo = createEntity()
-    applyTemplate(dodo, "Dodo")
-    component.Transform.getFor(dodo).position = component.Transform.getFor(spawner).position
+    local function spawnDodo()
+        local dodo = createChild(spawner, "spawned_dodo")
+        applyTemplate(dodo, "Dodo")
+        component.Transform.getFor(dodo).position = component.Transform.getFor(spawner).position
+    end
+
+    spawnDodo()
+
+    setUpdateFunction(spawner, 3, function(deltaTime)
+        if _G.player == nil or not valid(_G.player) then
+            return
+        end
+        local spawnerTransform = component.Transform.getFor(spawner)
+        local playerTransform = component.Transform.getFor(_G.player)
+
+        local posDiff = playerTransform.position - spawnerTransform.position
+        local distance = posDiff:length()
+        local spawnedDodo = getChild(spawner, "spawned_dodo")
+        if spawnedDodo == nil and distance > 40 then
+            spawnDodo()
+        end
+    end)
 end
 
