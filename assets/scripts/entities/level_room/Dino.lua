9993457977950db123d9ec0f0aee59fe9cdbacec
hilkojj <hilkojj@outlook.com>
2023-10-01 01:36:01 +0200

    Dino model

---
+++ assets/scripts/entities/level_room/Dino.lua
@@ -0,0 +1,45 @@
+
+masks = include("scripts/entities/level_room/_masks")
+
+loadModels("assets/models/dino.glb", false)
+
+persistenceMode(TEMPLATE | ARGS, {"Transform"})
+
+function create(dino)
+    setName(dino, "dino")
+
+    component.Transform.getFor(dino)
+
+    setComponents(dino, {
+        RenderModel {
+            modelName = "Dino",
+            visibilityMask = masks.NON_PLAYER
+        },
+        --[[
+        Rigged {
+            playingAnimations = {
+                PlayAnimation {
+                    name = "Walking",
+                    influence = 1,
+                    loop = true
+                }
+            }
+        },]]--
+        ShadowCaster(),
+        RigidBody {
+            mass = 0,
+            collider = Collider {
+                bounciness = 0,
+                frictionCoefficent = .3,
+                collisionCategoryBits = masks.STATIC_TERRAIN,
+                collideWithMaskBits = masks.DYNAMIC_CHARACTER | masks.PLAYER,
+                registerCollisions = true
+            }
+        },
+        SphereColliderShape {
+            radius = 0.8
+        }
+    })
+
+end
+
