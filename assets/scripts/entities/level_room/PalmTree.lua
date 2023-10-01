67ec151dc26766ccb1e69fc592a68ea22f58fec3
hilkojj <hilkojj@outlook.com>
2023-10-02 00:34:42 +0200

    instanced trees

--- assets/scripts/entities/level_room/PalmTree.lua
+++ assets/scripts/entities/level_room/PalmTree.lua
@@ -5,20 +5,33 @@ loadModels("assets/models/palmtree.glb", false)
 
 masks = include("scripts/entities/level_room/_masks")
 
-function create(palmtree)
-    setName(palmtree, "palmtree")
+local instancer = nil
+local i = 0
+
+function create(palmtree)i = i + 1
+    setName(palmtree, "palmtree"..i)
+
+    if instancer == nil then
+
+        instancer = createEntity()
+        setName(instancer, "tree instancer")
+
+        setComponents(instancer, {
+            InstancedRendering {
+            },
+            RenderModel {
+                modelName = "palmtree"
+            },
+            ShadowCaster(),
+            Transform()
+        })
+
+    end
 
     component.Transform.getFor(palmtree)
+    component.InstancedRendering.getFor(instancer):dirty().transformEntities:add(palmtree)
 
     setComponents(palmtree, {
-        RenderModel {
-            modelName = "palmtree",
-            visibilityMask = masks.NON_PLAYER
-        },
-        ShadowCaster(),
-        RigidBody {
-
-        },
         RigidBody {
             mass = 0,
             collider = Collider {
