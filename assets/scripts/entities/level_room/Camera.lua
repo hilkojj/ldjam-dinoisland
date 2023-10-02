47bc6343e3158f50741eca19c8f5bffaf3048e64
hilkojj <hilkojj@outlook.com>
2023-10-02 22:28:37 +0200

    sea sound, and volumes > 1 cause OpenAL errors

--- assets/scripts/entities/level_room/Camera.lua
+++ assets/scripts/entities/level_room/Camera.lua
@@ -14,7 +14,7 @@ function create(cam, args)
             nearClipPlane = .1,
             farClipPlane = 1000,
             visibilityMask = -1
-        },
+        }
     })
 
     component.Transform.getFor(cam)
@@ -24,9 +24,27 @@ function create(cam, args)
             setComponents(cam, { Transform() }) -- reset transform
         end
         setName(cam, args.name)
-        end
+    end
     if args.setAsMain then
         setMainCamera(cam)
     end
 
+    local seaSound = createEntity()
+
+    setUpdateFunction(cam, 0.5, function()
+        if getMainCamera() ~= cam then
+            return
+        end
+        if not component.SoundSpeaker.has(seaSound) then
+            setComponents(seaSound, {
+                SoundSpeaker {
+                    sound = "sounds/sea",
+                    volume = 0.0
+                }
+            })
+        end
+        local yDiff = component.Transform.getFor(cam).position.y - _G.seaHeight
+        local volume = math.max(0, math.min(1.0 - yDiff * 0.025))
+        component.SoundSpeaker.getFor(seaSound).volume = volume * 0.2
+    end)
 end
